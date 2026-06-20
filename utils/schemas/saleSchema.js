dc09f174f58da1df48e6ce324927812c41c1ca94
Denys Chafloque <161262844+Denys392@users.noreply.github.com>
2026-06-20 02:06:26 -0500

    refactor(sale): actualizar el esquema de ventas para mejorar la validación
    - Se renombra `userId` a `user_id` para mantener la consistencia con los demás esquemas.
    - Se añaden mensajes de error específicos para las validaciones `number.integer` y `number.positive` en `user_id` y `product_id`.
    - Se agrega un mensaje de validación para `string.empty` en el campo `document_type`.
    - Se incluyen mensajes para `number.base` y `array.base` en `quantity` e `items`, aportando mayor claridad sobre los tipos de datos esperados.

--- utils/schemas/saleSchema.js
+++ utils/schemas/saleSchema.js
@@ -1,9 +1,12 @@
 import Joi from "joi";
 
 export const createSaleSchema = Joi.object({
-  userId: Joi.number().integer().positive().allow(null).optional().messages({
+  user_id: Joi.number().integer().positive().allow(null).optional().messages({
     "number.base": "El ID del usuario debe ser un número entero válido.",
+    "number.integer": "El ID del usuario debe ser un número entero.",
+    "number.positive": "El ID del usuario debe ser un valor positivo.",
   }),
+
   document_type: Joi.string()
     .valid("BOLETA", "FACTURA", "TICKET", "BOLETA_WEB", "FACTURA_WEB")
     .required()
@@ -11,15 +14,20 @@ export const createSaleSchema = Joi.object({
       "any.only": "El tipo de comprobante no es válido.",
       "any.required":
         "El tipo de documento es obligatorio para procesar la venta.",
+      "string.empty": "El tipo de documento no puede estar vacío.",
     }),
+
   items: Joi.array()
     .items(
       Joi.object({
         product_id: Joi.number().integer().positive().required().messages({
           "number.base": "El ID del producto debe ser un número válido.",
+          "number.integer": "El ID del producto debe ser un número entero.",
+          "number.positive": "El ID del producto debe ser un valor positivo.",
           "any.required": "El ID del producto es obligatorio.",
         }),
         quantity: Joi.number().integer().min(1).required().messages({
+          "number.base": "La cantidad de artículos debe ser un valor numérico.",
           "number.integer":
             "La cantidad de artículos debe ser un número entero.",
           "number.min":
@@ -31,6 +39,8 @@ export const createSaleSchema = Joi.object({
     .min(1)
     .required()
     .messages({
+      "array.base":
+        "Los productos deben venir en un formato de lista estructurada.",
       "array.min":
         "El carrito de ventas debe contener al menos {#limit} producto.",
       "any.required": "La lista de productos es obligatoria para facturar.",
